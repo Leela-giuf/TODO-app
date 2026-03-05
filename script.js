a2b8911e5a53d933b046c7b4f3979b570b660b1f
Leela sri K.R <leelasriramesh15@gmail.com>
2026-03-05 18:53:58 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function addTask() {
+  const input = document.getElementById('taskInput');
+  const task = input.value.trim();
+
+  if (task === '') {
+    alert('Please enter a task!');
+    return;
+  }
+
+  const li = document.createElement('li');
+  li.innerHTML = `${task} <button onclick="deleteTask(this)">Delete</button>`;
+  
+  document.getElementById('taskList').appendChild(li);
+  input.value = '';
+}
+
+function deleteTask(button) {
+  button.parentElement.remove();
+}
